90715288d244f2d7b60c60e85a23587778dfc1df
Ericko9 <112918490+Ericko9@users.noreply.github.com>
2023-06-25 20:58:39 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,274 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+	<?php
+// Add this code at the beginning of the PHP section, before the HTML code
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['photo'])) {
+	$targetDir = 'uploads/'; // Define the directory where you want to store the uploaded images
+	$targetFile = $targetDir . basename($_FILES['photo']['name']);
+	$uploadOk = 1;
+	$imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+
+	// Check if the uploaded file is an image
+	$check = getimagesize($_FILES['photo']['tmp_name']);
+	if ($check !== false) {
+		$uploadOk = 1;
+	} else {
+		echo "File is not an image.";
+		$uploadOk = 0;
+	}
+
+	// Check if the file already exists
+	if (file_exists($targetFile)) {
+		echo "Sorry, file already exists.";
+		$uploadOk = 0;
+	}
+
+	// Check file size (optional)
+	if ($_FILES['photo']['size'] > 500000) {
+		echo "Sorry, your file is too large.";
+		$uploadOk = 0;
+	}
+
+	// Allow only specific file formats (optional)
+	if ($imageFileType != 'jpg' && $imageFileType != 'png' && $imageFileType != 'jpeg') {
+		echo "Sorry, only JPG, JPEG, and PNG files are allowed.";
+		$uploadOk = 0;
+	}
+
+	// If the file meets all the conditions, move it to the target directory
+	if ($uploadOk) {
+		if (move_uploaded_file($_FILES['photo']['tmp_name'], $targetFile)) {
+			// Save the file path to the database
+			$photoPath = $_FILES['photo']['name'];
+			$sql = "UPDATE voters SET photo = '$photoPath' WHERE voters_id = '".$voter['id']."'";
+			$conn->query($sql);
+			echo "The file has been uploaded and saved to the database.";
+		} else {
+			echo "Sorry, there was an error uploading your file.";
+		}
+	}
+}
+?>
+	<?php include 'includes/navbar.php'; ?>
+	 
+	  <div class="content-wrapper">
+	    <div class="container">
+
+	      <!-- Main content -->
+	      <section class="content">
+	      	<?php
+	      		$parse = parse_ini_file('admin/config.ini', FALSE, INI_SCANNER_RAW);
+    			$title = $parse['election_title'];
+	      	?>
+	      	<h1 class="page-header text-center title"><b><?php echo strtoupper($title); ?></b></h1>
+	        <div class="row">
+	        	<div class="col-sm-10 col-sm-offset-1">
+					<!-- Add this code within the <form> element -->
+	        		<?php
+				        if(isset($_SESSION['error'])){
+				        	?>
+				        	<div class="alert alert-danger alert-dismissible">
+				        		<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+					        	<ul>
+					        		<?php
+					        			foreach($_SESSION['error'] as $error){
+					        				echo "
+					        					<li>".$error."</li>
+					        				";
+					        			}
+					        		?>
+					        	</ul>
+					        </div>
+				        	<?php
+				         	unset($_SESSION['error']);
+
+				        }
+				        if(isset($_SESSION['success'])){
+				          	echo "
+				            	<div class='alert alert-success alert-dismissible'>
+				              		<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+				              		<h4><i class='icon fa fa-check'></i> Success!</h4>
+				              	".$_SESSION['success']."
+				            	</div>
+				          	";
+				          	unset($_SESSION['success']);
+				        }
+
+				    ?>
+ 
+				    <div class="alert alert-danger alert-dismissible" id="alert" style="display:none;">
+		        		<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+			        	<span class="message"></span>
+			        </div>
+
+				    <?php
+				    	$sql = "SELECT * FROM votes WHERE voters_id = '".$voter['id']."'";
+				    	$vquery = $conn->query($sql);
+				    	if($vquery->num_rows > 0){
+				    		?>
+				    		<div class="text-center">
+					    		<h3>Anda Telah Melakukan Pemilihan.</h3>
+					    		<a href="#view" data-toggle="modal" class="btn btn-flat btn-primary btn-lg">Pilihan Anda</a>
+					    	</div>
+				    		<?php
+				    	}
+				    	else{
+				    		?>
+			    			<!-- Voting Ballot -->
+						    <form method="POST" id="ballotForm" action="submit_ballot.php">
+<div class="form-group">
+	<label for="photo">WAJIB UPLOAD FOTO BUKTI DIRI:</label>
+	<input type="file" name="photo" id="photo">
+</div>
+				        		<?php
+				        			include 'includes/slugify.php';
+
+				        			$candidate = '';
+				        			$sql = "SELECT * FROM positions ORDER BY priority ASC";
+									$query = $conn->query($sql);
+									while($row = $query->fetch_assoc()){
+										$sql = "SELECT * FROM candidates WHERE position_id='".$row['id']."'";
+										$cquery = $conn->query($sql);
+										while($crow = $cquery->fetch_assoc()){
+											$slug = slugify($row['description']);
+											$checked = '';
+											if(isset($_SESSION['post'][$slug])){
+												$value = $_SESSION['post'][$slug];
+
+												if(is_array($value)){
+													foreach($value as $val){
+														if($val == $crow['id']){
+															$checked = 'checked';
+														}
+													}
+												}
+												else{
+													if($value == $crow['id']){
+														$checked = 'checked';
+													}
+												}
+											}
+											$input = ($row['max_vote'] > 1) ? '<input type="checkbox" class="flat-red '.$slug.'" name="'.$slug."[]".'" value="'.$crow['id'].'" '.$checked.'>' : '<input type="radio" class="flat-red '.$slug.'" name="'.slugify($row['description']).'" value="'.$crow['id'].'" '.$checked.'>';
+											$image = (!empty($crow['photo'])) ? 'images/'.$crow['photo'] : 'images/profile.jpg';
+											$candidate .= '
+												<li>
+													'.$input.'<button type="button" class="btn btn-primary btn-sm btn-flat clist platform" data-platform="'.$crow['platform'].'" data-fullname="'.$crow['firstname'].' '.$crow['lastname'].'"><i class="fa fa-search"></i> Platform</button><img src="'.$image.'" height="100px" width="100px" class="clist"><span class="cname clist">'.$crow['firstname'].' '.$crow['lastname'].'</span>
+												</li>
+											';
+										}
+
+										$instruct = ($row['max_vote'] > 1) ? 'You may select up to '.$row['max_vote'].' candidates' : 'Select only one candidate';
+
+										echo '
+											<div class="row">
+												<div class="col-xs-12">
+													<div class="box box-solid" id="'.$row['id'].'">
+														<div class="box-header with-border">
+															<h3 class="box-title"><b>'.$row['description'].'</b></h3>
+														</div>
+														<div class="box-body">
+															<p>'.$instruct.'
+																<span class="pull-right">
+																	<button type="button" class="btn btn-success btn-sm btn-flat reset" data-desc="'.slugify($row['description']).'"><i class="fa fa-refresh"></i> Reset</button>
+																</span>
+															</p>
+															<div id="candidate_list">
+																<ul>
+																	'.$candidate.'
+																</ul>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										';
+
+										$candidate = '';
+
+									}	
+
+				        		?>
+				        		<div class="text-center">
+					        		<button type="button" class="btn btn-success btn-flat" id="preview"><i class="fa fa-file-text"></i> Preview</button> 
+					        		<button type="submit" class="btn btn-primary btn-flat" name="vote"><i class="fa fa-check-square-o"></i> Submit</button>
+					        	</div>
+				        	</form>
+				        	<!-- End Voting Ballot -->
+				    		<?php
+				    	}
+
+				    ?>
+
+	        	</div>
+	        </div>
+	      </section>
+	     
+	    </div>
+	  </div>
+  
+  	<?php include 'includes/footer.php'; ?>
+  	<?php include 'includes/ballot_modal.php'; ?>
+</div>
+
+<?php include 'includes/scripts.php'; ?>
+<script>
+$(function(){
+	$('.content').iCheck({
+		checkboxClass: 'icheckbox_flat-green',
+		radioClass: 'iradio_flat-green'
+	});
+
+	$(document).on('click', '.reset', function(e){
+	    e.preventDefault();
+	    var desc = $(this).data('desc');
+	    $('.'+desc).iCheck('uncheck');
+	});
+
+	$(document).on('click', '.platform', function(e){
+		e.preventDefault();
+		$('#platform').modal('show');
+		var platform = $(this).data('platform');
+		var fullname = $(this).data('fullname');
+		$('.candidate').html(fullname);
+		$('#plat_view').html(platform);
+	});
+
+	$('#preview').click(function(e){
+		e.preventDefault();
+		var form = $('#ballotForm').serialize();
+		if(form == ''){
+			$('.message').html('You must vote atleast one candidate');
+			$('#alert').show();
+		}
+		else{
+			$.ajax({
+				type: 'POST',
+				url: 'preview.php',
+				data: form,
+				dataType: 'json',
+				success: function(response){
+					if(response.error){
+						var errmsg = '';
+						var messages = response.message;
+						for (i in messages) {
+							errmsg += messages[i]; 
+						}
+						$('.message').html(errmsg);
+						$('#alert').show();
+					}
+					else{
+						$('#preview_modal').modal('show');
+						$('#preview_body').html(response.list);
+					}
+				}
+			});
+		}
+		
+	});
+
+});
+</script>
+</body>
+</html>
